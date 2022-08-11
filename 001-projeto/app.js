67259c6e76d27fd4a3c03b92166dd705e66e898f
Edvard Agnapac <109630850+EdvardLDM@users.noreply.github.com>
2022-08-10 21:13:35 -0300

    Add files via upload

---
+++ 001-projeto/app.js
@@ -0,0 +1,8 @@
+var numero=20;
+var msg="teste";
+server.listen(port, hostname, () => {
+  //console.log(`Server running at http://${hostname}:${port}/`)
+})
+console.log(msg)
+console.log(typeof msg)
+console.log(numero)
