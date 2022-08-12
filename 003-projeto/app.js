c4860f13b262d90ba3a4bf3e63e65db27bdcf1d6
Edvard Agnapac <109630850+EdvardLDM@users.noreply.github.com>
2022-08-12 19:23:24 -0300

    Add files via upload

---
+++ 003-projeto/app.js
@@ -0,0 +1,8 @@
+var salario = 2000.00;
+var aumento = 0.15;
+var inflacao = 0.07;
+var novoSalario = salario + (aumento * salario);
+novoSalarioInflacao = novoSalario - (inflacao * novoSalario);
+
+console.log(novoSalario);
+console.log(novoSalarioInflacao);
